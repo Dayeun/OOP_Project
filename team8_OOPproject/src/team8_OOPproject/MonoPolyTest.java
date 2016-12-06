ff40b9bf56012b361e47a60e31d569c28cbc29c0
EunhyeAn <hjec3432@ajou.ac.kr>
2016-12-06 22:35:41 +0900

    061216

--- team8_OOPproject/src/team8_OOPproject/MonoPolyTest.java
+++ team8_OOPproject/src/team8_OOPproject/MonoPolyTest.java
@@ -140,26 +140,4 @@ public class MonoPolyTest extends JPanel implements ActionListener{
 		return jButton_reset;
 	}
 }
-class MonoPoly_Quiz extends JDialog{
-	private JLabel question;
-	 
-    public MonoPoly_Quiz(int ran){
-		Toolkit tk = Toolkit.getDefaultToolkit();
-		Dimension dim = tk.getScreenSize();
-		
-		this.setResizable(false);
-		this.setSize(600,400);
-		
-		int x = (dim.width / 2) - (this.getWidth() / 2);
-		int y = (dim.height / 2) - (this.getHeight() / 2);
-		this.setLocation(x, y);
-    	this.setTitle("Quiz");
-    	
-    	question = new JLabel("Your dice number is : "+ran);
-    	this.add(question);
-             
-        this.setModal(true);
-        this.setVisible(true);
-           
-    }
-}
+
