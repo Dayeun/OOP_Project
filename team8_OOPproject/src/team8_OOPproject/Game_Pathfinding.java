bfcca10b110af47ca7a831bfff69f4ae123a4790
김대한 <@DESKTOP-HLHS17N>
2016-12-09 00:28:20 +0900

    path finding u자 누르면 bar 움직이는거는 업어졌는데 흔들리는거는 아직 좀 있습니다

--- team8_OOPproject/src/team8_OOPproject/Game_Pathfinding.java
+++ team8_OOPproject/src/team8_OOPproject/Game_Pathfinding.java
@@ -1,12 +1,11 @@
 package team8_OOPproject;
 
 import java.awt.Color;
-import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import java.awt.event.MouseMotionListener;
+import java.awt.event.MouseMotionAdapter;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -14,7 +13,7 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-public class Game_Pathfinding extends JPanel implements MouseMotionListener,MouseListener,ActionListener{
+public class Game_Pathfinding extends JPanel implements ActionListener{
 	private JLabel bar_mg;
 	private JLabel u_mg;
 	private JButton rotation1;
@@ -41,9 +40,14 @@ public class Game_Pathfinding extends JPanel implements MouseMotionListener,Mous
 	int offX ,offY;
 
 	Game_Character sc;
-
+	Pathfinding_map s;
+	
 	public Game_Pathfinding()
 	{
+		
+		s = new Pathfinding_map();
+		int r_l = 98;
+		int u_d = 103;
 		sc =new Game_Character(105,290,"MainMagnet.PNG");
 
 		this.setSize(800,600);
@@ -70,13 +74,120 @@ public class Game_Pathfinding extends JPanel implements MouseMotionListener,Mous
 		this.add(sc);
 		this.add(jButton_reset);
 		this.add(jButton_next);
-	    
-		rotation1.addMouseListener(this);
-		rotation2.addMouseListener(this);
-		bar_mg.addMouseMotionListener(this);
-		bar_mg.addMouseListener(this);
-		u_mg.addMouseListener(this);
-		u_mg.addMouseMotionListener(this);
+		
+		bar_mg.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent e) {
+				offX = e.getX() - bar_mg.getX();   
+				offY = e.getY() - bar_mg.getY();
+			}
+			
+			public void mouseReleased(MouseEvent e){
+				int m;
+				int x = sc.getX() /110;   // Main magnet x position
+				int y = sc.getY() /300;   // Main magnet y position
+				int[] bar = new Game_PathfindingPosition().CheckPosition(bar_mg.getX(),bar_mg.getY(),bar_rotation);
+	
+				
+				if(bar[2] == 1){  
+					if((bar[0] == 0) && (y == bar[1])){
+						if(s.black_box(y) > x&&s.black_box(y)!= -1){
+							m = s.moveL_Magnet(y, x);
+					        for(int i = 0; i <m-1 ;i++)
+				    	    	sc.moving_L(sc.getX(),sc.getY(),r_l);
+						}			
+					}
+					else if((bar[0] == 6) && (y == bar[1])){ 
+						if(s.black_box(y) < x &&s.black_box(y)!= -1);
+			    			m = s.moveR_Magnet(y, x);
+				        	for(int i = 0; i <m-1 ;i++)
+					        	sc.moving_R(sc.getX(),sc.getY(),r_l);
+					}
+				}else if(bar[2] ==0){ 
+					if((bar[0] == 0) && ( y == bar[1])){
+						if(s.black_box(y) > x&&s.black_box(y)!= -1){
+					    	m = s.moveR_Magnet(y, x);
+				        	for(int i = 0; i <m-1 ;i++)
+					        	sc.moving_R(sc.getX(),sc.getY(),r_l);
+						}
+					}else if((bar[0] == 6) && (y == bar[1])){  
+						if(s.black_box(y) < x&&s.black_box(y)!= -1){
+							m = s.moveL_Magnet(y, x);
+					        for(int i = 0; i <m-1 ;i++)
+					        	sc.moving_L(sc.getX(),sc.getY(),r_l);
+						}		
+					}
+				}
+				if(s.checkGoal(y, x) == 1){
+					JOptionPane.showMessageDialog(null, "Success!", "Message", JOptionPane.INFORMATION_MESSAGE); // show
+				}
+			}
+		});
+		
+		bar_mg.addMouseMotionListener(new MouseMotionAdapter() {
+			public void mouseDragged(MouseEvent e) {
+				bar_mg.setLocation(e.getX()-offX, e.getY()-offY);
+			}
+		});
+		
+	
+		
+		u_mg.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent e) {
+		    	offX = e.getX() - u_mg.getX();   
+		    	offY = e.getY() - u_mg.getY();
+		    }	
+			
+			public void mouseReleased(MouseEvent e){
+				int x = sc.getX() /110;   // Main magnet x position
+				int y = sc.getY() /300;   // Main magnet y position
+				int[] u = new Game_PathfindingPosition().CheckPosition_(u_mg.getX(),u_mg.getY(),u_rotation);
+				
+				if(u[2] == 1){ 
+					if((x == u[0] - 1)&& (y == u[1] - 1 )){
+					    sc.moving_U(sc.getX(),sc.getY(),u_d);
+					}		  
+				}else if(u[2] == 0){ //위에서 아래로
+					if((x == u[0] - 1)&& (y == u[1])){
+					    sc.moving_D(sc.getX(),sc.getY(),u_d);
+					}	
+				}
+				
+				if(s.checkGoal(y, x) == 1){
+					JOptionPane.showMessageDialog(null, "Success!", "Message", JOptionPane.INFORMATION_MESSAGE); // show
+				}
+			}
+		});
+		
+		u_mg.addMouseMotionListener(new MouseMotionAdapter() {
+			public void mouseDragged(MouseEvent e) {
+				u_mg.setLocation(e.getX()-offX, e.getY()-offY);
+			}
+		});
+		
+		rotation1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(bar_rotation%2 == 0)
+					bar_mg.setIcon(icon1_1);
+				else
+					bar_mg.setIcon(icon1);			
+				bar_mg.setBounds(620,80,60,30);
+				bar_rotation++;				
+			}
+		});
+		
+		rotation2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(u_rotation %2 ==0)
+					u_mg.setIcon(icon2_1);
+				else
+					u_mg.setIcon(icon2);			
+				u_mg.setBounds(620, 150, 50,50);
+				u_rotation++;	
+			}
+		});
+		
 		jButton_reset.addActionListener(this);
 		jButton_next.addActionListener(this);
 		
@@ -96,167 +207,6 @@ public class Game_Pathfinding extends JPanel implements MouseMotionListener,Mous
 		this.setVisible(true);	
 	}
 	
-	@Override
-	public void mouseClicked(MouseEvent e) {
-		if(e.getSource().equals(rotation1)){
-			if(bar_rotation%2 == 0){
-				this.repaint();
-				this.remove(bar_mg);
-				bar_mg = new JLabel(icon1_1);
-				this.add(bar_mg);
-				this.repaint();
-				bar_mg.setVisible(true);
-				bar_mg.setBounds(620,80,60,30);
-				bar_rotation++;
-				bar_mg.addMouseMotionListener(this);
-				bar_mg.addMouseListener(this);
-				this.add(back);
-			}
-			else{
-				this.repaint();
-				this.remove(bar_mg);
-				bar_mg = new JLabel(icon1);
-				this.add(bar_mg);
-				bar_mg.setBounds(620,80,60,30);
-				bar_rotation++;
-				bar_mg.addMouseListener(this);
-				bar_mg.addMouseMotionListener(this);
-				this.add(back);
-			}
-		}
-		else if(e.getSource().equals(rotation2)){
-			if(u_rotation %2 ==0){
-				this.repaint();
-				this.remove(u_mg);
-				u_mg = new JLabel(icon2_1);
-				this.add(u_mg);
-				u_mg.setBounds(620, 150, 50,50);
-				u_rotation++;
-				u_mg.addMouseListener(this);
-				u_mg.addMouseMotionListener(this);
-				this.add(back);
-			}
-			else{
-				this.repaint();
-				this.remove(u_mg);
-				u_mg = new JLabel(icon2);
-				this.add(u_mg);
-				u_mg.setBounds(620, 150, 50,50);
-				u_rotation++;
-				u_mg.addMouseListener(this);
-				u_mg.addMouseMotionListener(this);
-				this.add(back);
-			}
-		}
-	}
-
-
-
-	@Override
-	public void mouseEntered(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-
-	@Override
-	public void mouseExited(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-
-	@Override
-	public void mousePressed(MouseEvent e) {
-		// TODO Auto-generated method stub
-		if(bar_mg.contains(new Point(e.getX(),e.getY()))){
-			isDragged1 = true;
-			offX = e.getX() - bar_mg.getX();   
-			offY = e.getY() - bar_mg.getY();
-		}
-		else if(u_mg.contains(new Point(e.getX(),e.getY()))){
-			isDragged2 = true;
-			offX = e.getX() - u_mg.getX();
-			offY = e.getY() - u_mg.getY();
-		}
-	}
-	
-	@Override
-	public void mouseDragged(MouseEvent e) {
-		// TODO Auto-generated method stub
-		if(isDragged1){
-	    	bar_mg.setLocation(e.getX()-offX, e.getY()-offY);
-		}else if(isDragged2){
-			u_mg.setLocation(e.getX()-offX, e.getY()-offY);
-		}
-		repaint();
-	}
-	
-	@Override
-	public void mouseReleased(MouseEvent e) {
-		// TODO Auto-generated method stub
-		isDragged1 = false;
-		isDragged2 = false;
-		int[] bar = new Game_PathfindingPosition().CheckPosition(bar_mg.getX(),bar_mg.getY(),bar_rotation);
-		int[] u = new Game_PathfindingPosition().CheckPosition_(u_mg.getX(),u_mg.getY(),u_rotation);
-		
-		int x, y;
-		x = sc.getX() /110;   // Main magnet x position
-		y = sc.getY() /300;   // Main magnet y position
-		
-		Pathfinding_map s = new Pathfinding_map();
-		int m;
-		int r_l = 98;
-		int u_d = 103;
-		
-		if(u[2] == 1){ 
-			if((x == u[0] - 1)&& (y == u[1] - 1 )){
-			    sc.moving_U(sc.getX(),sc.getY(),u_d);
-			}		  
-		}else if(u[2] == 0){ //위에서 아래로
-			if((x == u[0] - 1)&& (y == u[1])){
-			    sc.moving_D(sc.getX(),sc.getY(),u_d);
-			}	
-		}
-		
-		if(bar[2] == 1){  
-			if((bar[0] == 0) && (y == bar[1])){
-				if(s.black_box(y) > x&&s.black_box(y)!= -1){
-					m = s.moveL_Magnet(y, x);
-			        for(int i = 0; i <m-1 ;i++)
-		    	    	sc.moving_L(sc.getX(),sc.getY(),r_l);
-				}			
-			}
-			else if((bar[0] == 6) && (y == bar[1])){ 
-				if(s.black_box(y) < x &&s.black_box(y)!= -1);
-	    			m = s.moveR_Magnet(y, x);
-		        	for(int i = 0; i <m-1 ;i++)
-			        	sc.moving_R(sc.getX(),sc.getY(),r_l);
-			}
-		}else if(bar[2] ==0){ 
-			if((bar[0] == 0) && ( y == bar[1])){
-				if(s.black_box(y) > x&&s.black_box(y)!= -1){
-			    	m = s.moveR_Magnet(y, x);
-		        	for(int i = 0; i <m-1 ;i++)
-			        	sc.moving_R(sc.getX(),sc.getY(),r_l);
-				}
-			}else if((bar[0] == 6) && (y == bar[1])){  
-				if(s.black_box(y) < x&&s.black_box(y)!= -1){
-					m = s.moveL_Magnet(y, x);
-			        for(int i = 0; i <m-1 ;i++)
-			        	sc.moving_L(sc.getX(),sc.getY(),r_l);
-				}		
-			}
-		}
-		
-		if(s.checkGoal(y, x) == 1){
-			System.out.println(y +" "+ x );
-			JOptionPane.showMessageDialog(null, "Success!", "Message", JOptionPane.INFORMATION_MESSAGE); // show
-		}
-	}
-
-
-
 
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
@@ -268,11 +218,6 @@ public class Game_Pathfinding extends JPanel implements MouseMotionListener,Mous
 		
 	}
 
-	@Override
-	public void mouseMoved(MouseEvent arg0) {
-		// TODO Auto-generated method stub
-		
-	}
 
 	public JButton getjButton_back() {
 		return jButton_back;
